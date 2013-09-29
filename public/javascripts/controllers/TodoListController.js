530a9c945759618579f27af145a77cd098f2afee
Cesar Devers <cesar.devers@gmail.com>
2013-09-29 13:12:04 -0400

    made orbit.name and orbit.r.remainder in data model.GOING TO DO SORTING LOGIC FROM CONTROLLER

--- public/javascripts/controllers/TodoListController.js
+++ public/javascripts/controllers/TodoListController.js
@@ -3,11 +3,10 @@ var app= angular.module('TodoModule', ['ui.bootstrap']);
 function TodoListController($scope, $http) {
   $scope.bling=function(todo) {
     todo.done=! todo.done;
-    alert(todo.done);
+    $scope.update(todo);
+    todo.title="bob";
   }
-  $scope.cling=function() {
-    alert('yes!!');
-  }
+
   $scope.todos = [];
   $scope.newTodo = {
     title: '',
@@ -17,7 +16,7 @@ function TodoListController($scope, $http) {
     orbit: '',
     ttl:''
   };
-  $scope.orbits=[{orbit:5},{orbit:4},{orbit:3},{orbit:2},{orbit:1}];
+  $scope.orbits=[{name:5, remainder:0},{name:4, remainder:0},{name:3, remainder:0},{name:2, remainder:0},{name:1, remainder:0}];
 
   $scope.doneFilter = { done : true };
   $scope.notDoneFilter = { done : false };
@@ -27,12 +26,14 @@ function TodoListController($scope, $http) {
   };
 
   $scope.update = function(todo) {
+    console.log('calling update');
     $http.put('/todo/' + todo._id + '.json', todo).success(function(data) {
       if (!data.todo) {
         alert(JSON.stringify(data));
       }
     });
   };
+
 
   $scope.updateList = function() {
     $http.get('/todos.json').success(function(data) {
@@ -150,7 +151,7 @@ app.directive('planet', function() {
       size:"@",
       done:"@"
     },
-    template:"<div class='taskWrapper {{show}}' style='height:{{83+ttl*25}}px; margin-top:{{-10-12.5*ttl}}px;'><input type='checkbox' ng-model='todo.done' ng-change='update(todo)'><div class='taskPlanet {{size}}' style='width:{{50+25*ttl}}px; height:{{50+25*ttl}}px;'><div class='taskTTL'><h3>{{ttl}}</h3></div></div><div class='taskTitle'><h4 class='lead'>{{title}}</h4></div><div ng-show='showTaskMenu'><br /><button class='btn btn-danger'>Delete</button></div></div>",
+    template:"<div class='taskWrapper {{show}}' style='height:{{83+ttl*25}}px; margin-top:{{-10-12.5*ttl}}px;'><div class='taskPlanet {{size}}' style='width:{{50+25*ttl}}px; height:{{50+25*ttl}}px;'><div class='taskTTL'><h3>{{ttl}}</h3></div></div><div class='taskTitle'><h4 class='lead'>{{title}}</h4></div><div ng-show='showTaskMenu'><br /><button class='btn btn-danger'>Delete</button></div></div>",
     link:function(scope, element, attrs) {
       scope.showTaskMenu=false;
       scope.toggleTaskMenu=function() {
@@ -190,9 +191,9 @@ app.directive('planetRewrite', function() {
     scope:{
       ttl:"@",
       size:"@",
-      update:"&"
+      update:"&",
     },
-    template:"<div class='taskWrapper {{show}}' style='height:{{83+ttl*25}}px; margin-top:{{-10-12.5*tl}}px;'>{{todo.done}}<input type='checkbox' ng-model='todo.done', ng-change='update()' ><div class='taskPlanet {{size}}' style='width:{{50+25*ttl}}px; height:{{50+25*ttl}}px;'><div class='taskTTL'><h3>{{ttl}}</h3></div></div><div class='taskTitle'><h4 class='lead'>{{todo.title}}</h4></div><div ng-show='showTaskMenu'><br /><button class='btn btn-danger'>Delete</button></div></div>",
+    template:"<div class='taskWrapper {{show}} {{orbit}}' style='height:{{83+ttl*25}}px; margin-top:{{-10-12.5*tl}}px;'>{{todo.done}}<input type='checkbox' ng-model='todo.done', ng-change='update()' ><div class='taskPlanet {{size}}' style='width:{{50+25*ttl}}px; height:{{50+25*ttl}}px;'><div class='taskTTL'><h3>{{ttl}}</h3></div></div><div class='taskTitle'><h4 class='lead'>{{todo.title}}</h4></div><div ng-show='showTaskMenu'><br /><button class='btn btn-danger'>Delete</button></div></div>",
     link:function(scope, element, attrs) {
       scope.showTaskMenu=false;
       scope.toggleTaskMenu=function() {
@@ -200,6 +201,8 @@ app.directive('planetRewrite', function() {
         //element.css({'opacity':'0.5'});
 
       }
+
+      //assign planet illustration based on time to completion (TTL)
       attrs.$observe('ttl', function(value) {
         
         if(value<4) {
@@ -232,7 +235,7 @@ app.directive('sortOrbit', function() {
          scope.remainder=0;
          
          angular.forEach(scope.todos, function(todo) {
-           if(todo.orbit==scope.orbit.orbit) {
+           if(todo.orbit==scope.orbit.name) {
             scope.sum=scope.sum+parseInt(todo.ttl);
            }              
          })
@@ -241,13 +244,12 @@ app.directive('sortOrbit', function() {
             scope.remainder=8-scope.sum;
             angular.forEach(scope.todos, function(todo) {
                 //console.log("my current orbit is"+scope.orbit+"my remainder is"+scope.remainder+","+"now i'm going up to"+parseInt(scope.orbit+1)+"and the ttls here are"+todo.ttl);
-                if(todo.orbit==(scope.orbit.orbit+1)) {
+                if(todo.orbit==(scope.orbit.name+1)) {
                   if(todo.ttl==scope.remainder){
-                    alert('current orbit'+scope.orbit.orbit+todo.title+todo.orbit.orbit);  
-                                     
+                    //alert('current orbit'+scope.orbit.orbit+todo.title+todo.orbit);    
                   }
                   else{
-                    console.log('my orbit is'+todo.orbit+'im coming up from'+scope.orbit.orbit+"my ttl is "+todo.ttl);
+                    console.log('my orbit is'+todo.orbit+'im coming up from'+scope.orbit.name+"my ttl is "+todo.ttl);
                   }  
                 }
 
